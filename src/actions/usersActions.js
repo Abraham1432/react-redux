5c89749e6192ac623c1a567f73a93c3d25076b96
Abraham Gomez <gomezabraham1432@gmail.com>
2021-01-26 22:29:54 -0800

    se agregaron estados para el control de errores y el manejo de datos

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,13 +2,30 @@ import axios  from "axios";
 
 export const get_users = () => async(dispatch) => {
 
-      // const url = "https:\\superheroapi.com/api/10158053293211733";
-    const url = "https://jsonplaceholder.typicode.com/users";
-    const api =  await axios.get(url)
-
     dispatch({
-        type:'get_users',
-        payload:api.data
+        type:"loading"
     })
+
+    try{
+     //const url = "https://www.superheroapi.com/api.php/10158053293211733/search/ironman";
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const api =  await axios.get(url)
+    
+      dispatch({
+          type:'get_users',
+          payload:api.data
+      })
+
+    } catch(error){
+
+        console.log(error);
+
+        dispatch({
+            type:'error',
+            payload:error.message
+        })
+
+    }
+
 
 }
--- src/reducers/user_reduce.js
+++ src/reducers/user_reduce.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 const INITIAL_STATE = {
 
-    users:[]
+    users:[],
+    loading:false,
+    error:""
 
 }
 
@@ -9,8 +11,15 @@ export default (state = INITIAL_STATE,action) =>{
     switch (action.type) {
         case 'get_users':
             
-            return { ...state, users:action.payload}
+            return { ...state, users:action.payload,loading:false};
+        case 'loading':
+        
+            return { ...state, loading:true}
+
+        case 'error':
     
+            return { ...state,error:action.payload,loading:false}
+
         default: return state;
     }
 }
